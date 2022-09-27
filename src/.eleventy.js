ec9cc0c2e2a95a56e4d08cd45dc46c12ae43e7b4
Shawn Wildermuth <shawn@wildermuth.com>
2022-09-27 15:16:06 -0400

    Update to fix pod

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -14,14 +14,14 @@ module.exports = function (cfg) {
 
   if (process.env.NODE_ENV !== "production") {
     const ignores = [
-      "input/hwpod/",
-      "input/music/",
+      //"input/hwpod/",
+      //"input/music/",
       // "input/search/",
       // "input/videos/",
       //"input/tags.njk",
       //"input/*.njk",
-      "input/stories/200*/**/",
-      "input/stories/201*/**/",
+      //"input/stories/200*/**/",
+      //"input/stories/201*/**/",
       //"input/stories/202*/",
     ];
     for (let ignore of ignores) {
